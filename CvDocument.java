082550eb47e0a5b6f5db629110bf9552114a3aec
Lee-ShauLun <2296193659@qq.com>
2026-04-07 00:25:28 +0800

    Add files via upload
    The properties of the resume in the system

---
+++ CvDocument.java
@@ -0,0 +1,71 @@
+package cn.edu.bupt.tarecruitment.model;
+
+public class CvDocument {
+
+    private String id;
+    private String applicantId;
+    private String originalFileName;
+    private String storedFileName;
+    private String contentType;
+    private long size;
+    private String uploadedAt;
+
+    public CvDocument() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getApplicantId() {
+        return applicantId;
+    }
+
+    public void setApplicantId(String applicantId) {
+        this.applicantId = applicantId;
+    }
+
+    public String getOriginalFileName() {
+        return originalFileName;
+    }
+
+    public void setOriginalFileName(String originalFileName) {
+        this.originalFileName = originalFileName;
+    }
+
+    public String getStoredFileName() {
+        return storedFileName;
+    }
+
+    public void setStoredFileName(String storedFileName) {
+        this.storedFileName = storedFileName;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public String getUploadedAt() {
+        return uploadedAt;
+    }
+
+    public void setUploadedAt(String uploadedAt) {
+        this.uploadedAt = uploadedAt;
+    }
+}
